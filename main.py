13eeaa3df8949ca12eedbbc5daa4f424f3ecce28
Alex <alex@likening.co.uk>
2022-11-10 11:42:21 +0000

    Init Commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import os
+
+modules_not_found = []
+
+try:
+    import urllib
+except ModuleNotFoundError as e:
+    modules_not_found.append(str(str(e).split('\'')[1]))
+
+try:
+    import bs4
+except ModuleNotFoundError as e:
+    modules_not_found.append(str(str(e).split('\'')[1]))
+
+for item in modules_not_found:
+    os.system(f'pip install {item}')
+
+import urllib.request as ul
+from bs4 import BeautifulSoup as soup
+
+##
+## Got to think about bes tway to automate getting bets for sports, and the leagues in those sports
+##  E.g Premier league in football
+##
+
+tournaments = ['premier-league', 'uefa-champions-league']
+
+for t in tournaments:
+
+    print("\n ------------- ( " + t + " ) -----------------")
+
+    football_url = "https://easyodds.com/football/" + t
+    req = ul.Request(football_url)
+    client = ul.urlopen(req)
+    htmldata = client.read()
+    client.close()
+
+    pagesoup = soup(htmldata, "html.parser")
+    itemlocator = pagesoup.findAll('div', {"class": "tournament-event__row"})
+
+    bets = {}
+
+    for item in itemlocator:
+
+        home = item.find_next('div', {'class': 'tournament-event__cell event-team event-team-home'})
+        draw = item.find_next('div', {'class': 'tournament-event__cell event-draw'})
+        away = item.find_next('div', {'class': 'tournament-event__cell event-team event-team-away'})
+
+        home_name = (home.find_next('div', {'class': 'event-team__name'})).text.strip()
+        home_bet = (home.find_next('span', {'class': 'odds-button'})).text.strip()
+
+        draw_bet = (draw.find_next('span', {'class': 'odds-button'})).text.strip()
+
+        away_name = (away.find_next('div', {'class': 'event-team__name'})).text.strip()
+        away_bet = (away.find_next('span', {'class': 'odds-button'})).text.strip()
+
+        print(home_name + " vs " + away_name)
+        print(home_bet + " " + draw_bet + " " + away_bet)
+
+
+
