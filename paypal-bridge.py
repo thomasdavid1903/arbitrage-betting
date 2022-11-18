fbfc04d320049ef8b0078b8205592f7ffdaca508
Alex <alex@likening.co.uk>
2022-11-18 11:12:18 +0000

    created paypal bridge

---
+++ paypal-bridge.py
@@ -0,0 +1,21 @@
+#
+# Copyright (C) Alex King - All Rights Reserved
+#
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+# 
+# Created on DATE by USER
+#
+
+class PayPal:
+
+    def __int__(self):
+        pass
+
+
+
+def main():
+    ...
+
+if __name__ == "__main__":
+    main()
