8d4957cea7684fee10740e29aedea93700d6f2b4
Alex <alex@likening.co.uk>
2022-11-18 14:27:14 +0000

    TOMMY MAKE SURE YOU PULL SLAG - should be fixed now

--- bets.py
+++ bets.py
@@ -2,49 +2,45 @@ import urllib.request as ul
 from bs4 import BeautifulSoup as soup
 
 
-def get_bets():
-    tournaments = ['premier-league', 'uefa-champions-league']
+def get_bets(tournament, verbose=False):
 
-    bets_tournaments = {}
+    if verbose:
+        print("\n ------------- ( " + tournament + " ) -----------------")
 
-    for t in tournaments:
+    football_url = "https://easyodds.com/football/" + tournament
 
-        print("\n ------------- ( " + t + " ) -----------------")
+    req = ul.Request(football_url)
 
-        football_url = "https://easyodds.com/football/" + t
-        req = ul.Request(football_url)
-        client = ul.urlopen(req)
-        htmldata = client.read()
-        client.close()
+    client = ul.urlopen(req)
+    htmldata = client.read()
+    client.close()
 
-        pagesoup = soup(htmldata, "html.parser")
-        itemlocator = pagesoup.findAll('div', {"class": "tournament-event__row"})
+    pagesoup = soup(htmldata, "html.parser")
+    itemlocator = pagesoup.findAll('div', {"class": "tournament-event__row"})
 
-        bets = {}
+    bets = {}
 
-        for item in itemlocator:
-            home = item.find_next('div', {'class': 'tournament-event__cell event-team event-team-home'})
-            draw = item.find_next('div', {'class': 'tournament-event__cell event-draw'})
-            away = item.find_next('div', {'class': 'tournament-event__cell event-team event-team-away'})
+    for item in itemlocator:
+        home = item.find_next('div', {'class': 'tournament-event__cell event-team event-team-home'})
+        draw = item.find_next('div', {'class': 'tournament-event__cell event-draw'})
+        away = item.find_next('div', {'class': 'tournament-event__cell event-team event-team-away'})
 
-            home_name = (home.find_next('div', {'class': 'event-team__name'})).text.strip()
-            home_bet = (home.find_next('span', {'class': 'odds-button'})).text.strip()
+        home_name = (home.find_next('div', {'class': 'event-team__name'})).text.strip()
+        home_bet = (home.find_next('span', {'class': 'odds-button'})).text.strip()
 
-            draw_bet = (draw.find_next('span', {'class': 'odds-button'})).text.strip()
+        draw_bet = (draw.find_next('span', {'class': 'odds-button'})).text.strip()
 
-            away_name = (away.find_next('div', {'class': 'event-team__name'})).text.strip()
-            away_bet = (away.find_next('span', {'class': 'odds-button'})).text.strip()
+        away_name = (away.find_next('div', {'class': 'event-team__name'})).text.strip()
+        away_bet = (away.find_next('span', {'class': 'odds-button'})).text.strip()
 
+        if verbose:
             print(home_name + " vs " + away_name)
             print(home_bet + " " + draw_bet + " " + away_bet)
 
-            # Figure out return format for bets
-            bets[home_name + " vs " + away_name] = [home_bet, draw_bet, away_bet]
+        # Figure out return format for bets
+        bets[home_name + " vs " + away_name] = [home_bet, draw_bet, away_bet]
 
-        bets_tournaments[t] = bets
-
-    print(bets_tournaments)
-    return bets_tournaments
+    return bets
 
 def get_odds():
     tournaments = ['premier-league', 'uefa-champions-league']
--- paypal_bridge.py
+++ paypal_bridge.py
@@ -9,14 +9,30 @@
 
 import urllib.request as ul
 
+
 class PayPal:
+    """
+    PayPal API:
+        - Uses HTTP posting to communicate with paypal via web
+        - Posting requires credentials
+        - Researching how to post to paypal in order to transfer money
+
+    Functions:
+        - Send Money (Via address)
+        - Check Balance
+        - Get recent transactions
+
+    """
 
     def __int__(self):
         pass
 
+    def call(self):
+        ...
+
 
 def main():
-    ...
+    pass
 
 
 if __name__ == "__main__":
