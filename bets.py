1992d00f86cf2a813390d5b6adb9d9edeecaff2c
tommy <thomasdavid190303@gmail.com>
2022-11-18 13:23:51 +0000

    maths tings

--- bets.py
+++ bets.py
@@ -2,43 +2,47 @@ import urllib.request as ul
 from bs4 import BeautifulSoup as soup
 
 
-def get_bets(tournament, verbose=False):
+def get_bets():
+    tournaments = ['premier-league', 'uefa-champions-league']
 
-    if verbose:
-        print("\n ------------- ( " + tournament + " ) -----------------")
+    bets_tournaments = {}
 
-    football_url = "https://easyodds.com/football/" + tournament
+    for t in tournaments:
 
-    req = ul.Request(football_url)
+        print("\n ------------- ( " + t + " ) -----------------")
 
-    client = ul.urlopen(req)
-    htmldata = client.read()
-    client.close()
+        football_url = "https://easyodds.com/football/" + t
+        req = ul.Request(football_url)
+        client = ul.urlopen(req)
+        htmldata = client.read()
+        client.close()
 
-    pagesoup = soup(htmldata, "html.parser")
-    itemlocator = pagesoup.findAll('div', {"class": "tournament-event__row"})
+        pagesoup = soup(htmldata, "html.parser")
+        itemlocator = pagesoup.findAll('div', {"class": "tournament-event__row"})
 
-    bets = {}
+        bets = {}
 
-    for item in itemlocator:
-        home = item.find_next('div', {'class': 'tournament-event__cell event-team event-team-home'})
-        draw = item.find_next('div', {'class': 'tournament-event__cell event-draw'})
-        away = item.find_next('div', {'class': 'tournament-event__cell event-team event-team-away'})
+        for item in itemlocator:
+            home = item.find_next('div', {'class': 'tournament-event__cell event-team event-team-home'})
+            draw = item.find_next('div', {'class': 'tournament-event__cell event-draw'})
+            away = item.find_next('div', {'class': 'tournament-event__cell event-team event-team-away'})
 
-        home_name = (home.find_next('div', {'class': 'event-team__name'})).text.strip()
-        home_bet = (home.find_next('span', {'class': 'odds-button'})).text.strip()
+            home_name = (home.find_next('div', {'class': 'event-team__name'})).text.strip()
+            home_bet = (home.find_next('span', {'class': 'odds-button'})).text.strip()
 
-        draw_bet = (draw.find_next('span', {'class': 'odds-button'})).text.strip()
+            draw_bet = (draw.find_next('span', {'class': 'odds-button'})).text.strip()
 
-        away_name = (away.find_next('div', {'class': 'event-team__name'})).text.strip()
-        away_bet = (away.find_next('span', {'class': 'odds-button'})).text.strip()
+            away_name = (away.find_next('div', {'class': 'event-team__name'})).text.strip()
+            away_bet = (away.find_next('span', {'class': 'odds-button'})).text.strip()
 
-        if verbose:
             print(home_name + " vs " + away_name)
             print(home_bet + " " + draw_bet + " " + away_bet)
 
-        # Figure out return format for bets
-        bets[home_name + " vs " + away_name] = [home_bet, draw_bet, away_bet]
+            # Figure out return format for bets
+            bets[home_name + " vs " + away_name] = [home_bet, draw_bet, away_bet]
 
-    return bets
+        bets_tournaments[t] = bets
+
+    print(bets_tournaments)
+    return bets_tournaments
 
--- main.py
+++ main.py
@@ -1,40 +1,15 @@
 #
 # Got to think about bes tway to automate getting bets for sports, and the leagues in those sports
 #  E.g Premier league in football
-#
+# i reckon at the moment we just focus on getting a program where it takes the bets from the website and reports back the profitable matches and the possible bets
+## and worry about the automation later
 
 from bets import get_bets
-from core import points
-from paypal_bridge import PayPal
+# this is a test, alex is a queer
 
-
+# tommy like dick in his ass
 def main():
-    #tournament = get_bets(tournament='premier-league')
-    #tournament = get_bets(tournament='uefa-champions-league')
-    tournament = get_bets(tournament='world-cup')
-
-    for matches in tournament:
-        print(matches)
-        bets = tournament[matches]
-
-        bet1 = bets[0]
-        bet1 = float(bet1.split("/")[0]) / float(bet1.split("/")[1])
-
-        bet2 = bets[1]
-        bet2 = float(bet2.split("/")[0]) / float(bet2.split("/")[1])
-
-        bet3 = bets[2]
-        bet3 = float(bet3.split("/")[0]) / float(bet3.split("/")[1])
-
-        points(bet1, bet2, bet3, precision=2)
-
-        # Tommy
-
-        # 'points' needs to return something that can be used to decide whether a bet should be made or not
-        # and what team to bet on
-
-        return
-
+    t = get_bets()
 
 if __name__ == "__main__":
     main()
