00f5c1f4df750d063f24df2ae2dc712eabfae725
Alex <alex@likening.co.uk>
2022-11-18 11:12:06 +0000

    Select tournament

--- bets.py
+++ bets.py
@@ -2,48 +2,43 @@ import urllib.request as ul
 from bs4 import BeautifulSoup as soup
 
 
-def get_bets(verbose = False):
-    tournaments = ['premier-league', 'uefa-champions-league']
+def get_bets(tournament, verbose=False):
 
-    bets_tournaments = {}
+    if verbose:
+        print("\n ------------- ( " + tournament + " ) -----------------")
 
-    for t in tournaments:
+    football_url = "https://easyodds.com/football/" + tournament
+
+    req = ul.Request(football_url)
+
+    client = ul.urlopen(req)
+    htmldata = client.read()
+    client.close()
+
+    pagesoup = soup(htmldata, "html.parser")
+    itemlocator = pagesoup.findAll('div', {"class": "tournament-event__row"})
+
+    bets = {}
+
+    for item in itemlocator:
+        home = item.find_next('div', {'class': 'tournament-event__cell event-team event-team-home'})
+        draw = item.find_next('div', {'class': 'tournament-event__cell event-draw'})
+        away = item.find_next('div', {'class': 'tournament-event__cell event-team event-team-away'})
+
+        home_name = (home.find_next('div', {'class': 'event-team__name'})).text.strip()
+        home_bet = (home.find_next('span', {'class': 'odds-button'})).text.strip()
+
+        draw_bet = (draw.find_next('span', {'class': 'odds-button'})).text.strip()
+
+        away_name = (away.find_next('div', {'class': 'event-team__name'})).text.strip()
+        away_bet = (away.find_next('span', {'class': 'odds-button'})).text.strip()
 
         if verbose:
-            print("\n ------------- ( " + t + " ) -----------------")
+            print(home_name + " vs " + away_name)
+            print(home_bet + " " + draw_bet + " " + away_bet)
 
-        football_url = "https://easyodds.com/football/" + t
-        req = ul.Request(football_url)
-        client = ul.urlopen(req)
-        htmldata = client.read()
-        client.close()
+        # Figure out return format for bets
+        bets[home_name + " vs " + away_name] = [home_bet, draw_bet, away_bet]
 
-        pagesoup = soup(htmldata, "html.parser")
-        itemlocator = pagesoup.findAll('div', {"class": "tournament-event__row"})
-
-        bets = {}
-
-        for item in itemlocator:
-            home = item.find_next('div', {'class': 'tournament-event__cell event-team event-team-home'})
-            draw = item.find_next('div', {'class': 'tournament-event__cell event-draw'})
-            away = item.find_next('div', {'class': 'tournament-event__cell event-team event-team-away'})
-
-            home_name = (home.find_next('div', {'class': 'event-team__name'})).text.strip()
-            home_bet = (home.find_next('span', {'class': 'odds-button'})).text.strip()
-
-            draw_bet = (draw.find_next('span', {'class': 'odds-button'})).text.strip()
-
-            away_name = (away.find_next('div', {'class': 'event-team__name'})).text.strip()
-            away_bet = (away.find_next('span', {'class': 'odds-button'})).text.strip()
-
-            if verbose:
-                print(home_name + " vs " + away_name)
-                print(home_bet + " " + draw_bet + " " + away_bet)
-
-            # Figure out return format for bets
-            bets[home_name + " vs " + away_name] = [home_bet, draw_bet, away_bet]
-
-        bets_tournaments[t] = bets
-
-    return bets_tournaments
+    return bets
 
--- main.py
+++ main.py
@@ -8,23 +8,26 @@ from core import points
 
 
 def main():
-    tournaments = get_bets()
+    #tournament = get_bets(tournament='premier-league')
+    #tournament = get_bets(tournament='uefa-champions-league')
+    tournament = get_bets(tournament='world-cup')
 
-    for league in tournaments:
+    for matches in tournament:
+        print(matches)
+        bets = tournament[matches]
 
-        for matches in tournaments[league]:
-            bets = tournaments[league][matches]
+        bet1 = bets[0]
+        bet1 = float(bet1.split("/")[0]) / float(bet1.split("/")[1])
 
-            bet1 = bets[0]
-            bet1 = float(bet1.split("/")[0]) / float(bet1.split("/")[1])
+        bet2 = bets[1]
+        bet2 = float(bet2.split("/")[0]) / float(bet2.split("/")[1])
 
-            bet2 = bets[1]
-            bet2 = float(bet2.split("/")[0]) / float(bet2.split("/")[1])
+        bet3 = bets[2]
+        bet3 = float(bet3.split("/")[0]) / float(bet3.split("/")[1])
 
-            bet3 = bets[2]
-            bet3 = float(bet3.split("/")[0]) / float(bet3.split("/")[1])
+        points(bet1, bet2, bet3, precision=2)
 
-            points(bet1, bet2, bet3, precision=1)
+        return
 
 
 if __name__ == "__main__":
